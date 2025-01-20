4b6af4840ce82694fa04a9998381220f2fb4d9b6
theunmenger <163689434+theunmenger@users.noreply.github.com>
2025-01-20 12:59:33 +0100

    opgave gemaakt

---
+++ hoger of lager/script.js
@@ -0,0 +1,16 @@
+let number = Math.floor(Math.random() * 11);
+
+
+document.getElementById('submit').onclick = function() {
+    let number_guessed = document.getElementById('number_guessed').value;
+    console.log(number);
+    if (number_guessed < number) {
+        alert("too low");
+    }   else if (number_guessed > number) {
+        alert("too high");
+    } else if (number == number_guessed) {
+        alert("you win");
+        document.getElementById('number_guessed').value = '';
+        location.reload();
+    }
+}
